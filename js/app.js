e62ee0da170eed457e5a9764c32dd9294f8adf4b
Svetozar <svetozariliev13@gmail.com>
2020-04-30 20:00:37 +0300

    fix project to run without live server

--- js/app.js
+++ js/app.js
@@ -1,31 +1,33 @@
-const url = 'https://restcountries.eu/rest/v2/all?fields=name';
-let countriesArrayWithObjects = [];
+function start() {
+  const url = 'https://restcountries.eu/rest/v2/all?fields=name';
+  let countriesArrayWithObjects = [];
 
-fetch(url)
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    countriesArrayWithObjects = data;
-    return showCountries(data);
-  })
-  .catch(error => console.log(error));
+  fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      countriesArrayWithObjects = data;
+      return showCountries(data);
+    })
+    .catch(error => console.log(error));
 
-function showCountries(data) {
-  let createDivElements = "";
-  data.forEach(obj => {
-    createDivElements += `<div class="country-name">${obj.name}</div>`
-  });
-  const countryHolderElement = document.getElementById("country-holder");
-  return countryHolderElement.innerHTML = createDivElements;
-}
+  function showCountries(data) {
+    let createDivElements = "";
+    data.forEach(obj => {
+      createDivElements += `<div class="country-name">${obj.name}</div>`
+    });
+    const countryHolderElement = document.getElementById("country-holder");
+    return countryHolderElement.innerHTML = createDivElements;
+  }
 
-const inputField = document.getElementById("input");
-inputField.addEventListener("input", searchAndShowFoundCountries);
+  const inputField = document.getElementById("input");
+  inputField.addEventListener("input", searchAndShowFoundCountries);
 
-function searchAndShowFoundCountries() {
-  const userInput = this.value;
-  const regExp = new RegExp(userInput, "gi");
-  const serchResultsArray = countriesArrayWithObjects.filter(obj => obj.name.match(regExp));
-  return showCountries(serchResultsArray);
+  function searchAndShowFoundCountries() {
+    const userInput = this.value;
+    const regExp = new RegExp(userInput, "gi");
+    const serchResultsArray = countriesArrayWithObjects.filter(obj => obj.name.match(regExp));
+    return showCountries(serchResultsArray);
+  }
 }
