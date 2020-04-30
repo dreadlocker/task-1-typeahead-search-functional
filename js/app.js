b65afaaafc5e3c3c43be41a09f5b9fd4f4b10040
Svetozar <svetozariliev13@gmail.com>
2020-04-30 19:08:33 +0300

    whole project

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const url = 'https://restcountries.eu/rest/v2/all?fields=name';
+let countriesArrayWithObjects = [];
+
+fetch(url)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    countriesArrayWithObjects = data;
+    return showCountries(data);
+  })
+  .catch(error => console.log(error));
+
+function showCountries(data) {
+  let createDivElements = "";
+  data.forEach(obj => {
+    createDivElements += `<div class="country-name">${obj.name}</div>`
+  });
+  const countryHolderElement = document.getElementById("country-holder");
+  return countryHolderElement.innerHTML = createDivElements;
+}
+
+const inputField = document.getElementById("input");
+inputField.addEventListener("input", searchAndShowFoundCountries);
+
+function searchAndShowFoundCountries() {
+  const userInput = this.value;
+  const regExp = new RegExp(userInput, "gi");
+  const serchResultsArray = countriesArrayWithObjects.filter(obj => obj.name.match(regExp));
+  return showCountries(serchResultsArray);
+}
